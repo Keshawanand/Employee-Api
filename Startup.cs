414a9afcc8800a6d9362c396f079a8c56e62104f
Keshawanand Singh <41300480+Keshawanand@users.noreply.github.com>
2022-09-04 18:01:24 +0530

    Add Swagger

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,7 @@ namespace EmployeeApi
         public void ConfigureServices(IServiceCollection services)
         {
             var connection = Configuration.GetConnectionString("EmployeeDB");
+
             services.AddMvc()
               .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
             services.AddDbContextPool<EmployeeDBContext>(options => options.UseSqlServer(connection));
@@ -43,6 +45,43 @@ namespace EmployeeApi
                 });
             });
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Authentication API",
+                    Description = ""
+                });
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer x43fjhjx34565\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+            });
+
 
             services.AddAuthentication(option =>
             {
@@ -61,6 +100,7 @@ namespace EmployeeApi
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("jwt:Key")))
                 };
                 });
+            //services.AddSwaggerGen();
             services.AddControllers(); 
 
         }
@@ -79,6 +119,11 @@ namespace EmployeeApi
             app.UseAuthentication();
 
             app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
